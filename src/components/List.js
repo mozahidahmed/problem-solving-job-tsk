0044d08a4c0fa4778a0dfa4071addebd946a405a
mozahidsani <mozahidwebmin11d10@gmail.com>
2022-09-27 21:04:55 +0600

    complete

--- src/components/List.js
+++ src/components/List.js
@@ -4,19 +4,26 @@ import SingleListItem from './SingleListItem';
 
 
 const WrappedListComponent = ({ items }) => {
-
     const [selectedIndex, setSelectedIndex] = useState();
 
+
+// useEffect
     useEffect(() => {
         setSelectedIndex(null);
     }, [items, selectedIndex]);
 
+    // handle 
     const handleClick = index => {
         setSelectedIndex(index);
     };
 
+
+
     return (
+        // ul 
         <ul style={{ textAlign: 'left' , width: '200px', margin: '0 auto'}}>
+           
+            {/* map */}
             {items.map((item, index) => (
                 <SingleListItem
                     key={index}
@@ -26,16 +33,23 @@ const WrappedListComponent = ({ items }) => {
                     isSelected={selectedIndex}
                 />
             ))}
+         {/* map */}
+
         </ul>
     )
 };
 
 
+
+
+
+
+// ........................................
 WrappedListComponent.defaultProps = {
     items: null,
 };
 
 const List = memo(WrappedListComponent);
-
+// ............................................
 export default List;
 
--- src/components/SingleListItem.js
+++ src/components/SingleListItem.js
@@ -1,22 +1,30 @@
 import React, { memo } from 'react';
 import PropTypes from 'prop-types';
 
+
 const WrappedSingleListItem = ({
     index,
     isSelected,
     onClickHandler,
     text,
 }) => {
+
     return (
         <li
+
             style={{ backgroundColor: isSelected ? 'green' : 'red' , margin: "10px", padding: '5px'}}
             onClick={() => onClickHandler(index)}
         >
             {text}
         </li>
     );
+
 };
 
+
+
+
+// /..............................................
 WrappedSingleListItem.propTypes = {
     index: PropTypes.number,
     isSelected: PropTypes.number,
@@ -25,5 +33,6 @@ WrappedSingleListItem.propTypes = {
 };
 
 const SingleListItem = memo(WrappedSingleListItem);
+// ........................................................
 
 export default SingleListItem;
